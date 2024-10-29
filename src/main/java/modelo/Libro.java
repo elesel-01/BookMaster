714369e456ad69f7f1712461eaee3f822a895786
Gerardo <gerarleonce-0703@outlook.es>
2024-10-29 00:03:57 -0500

    Se añadieron las carpetas faltantes de src/main y src/test

--- src/main/java/modelo/Libro.java
+++ src/main/java/modelo/Libro.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Libro {
 	private String titulo;
-	private String autor; //cambiando tipo Autor a String
+	private String autor; // cambiando tipo Autor a String
 	private int id;
 	private boolean disponible;
 	private String editorial;
@@ -20,68 +20,68 @@ public class Libro {
 		this.anioPublicacion = anioPublicacion;
 		this.categoria = categoria;
 
-		//autor.agregarObra(this); no entiendo.
+		// autor.agregarObra(this); no entiendo.
 	}
-	
+
 	/**************** Getters y Setters *********************/
 
-    public int getId() {
-        return id;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    public String getTitulo() {
-        return titulo;
-    }
+	public String getTitulo() {
+		return titulo;
+	}
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
 
-    public String getAutor() {
-        return autor;
-    }
+	public String getAutor() {
+		return autor;
+	}
 
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
 
-    public String getEditorial() {
-        return editorial;
-    }
+	public String getEditorial() {
+		return editorial;
+	}
 
-    public void setEditorial(String editorial) {
-        this.editorial = editorial;
-    }
+	public void setEditorial(String editorial) {
+		this.editorial = editorial;
+	}
 
-    public int getAnioPublicacion() {
-        return anioPublicacion;
-    }
+	public int getAnioPublicacion() {
+		return anioPublicacion;
+	}
 
-    public void setAnioPublicacion(int anioPublicacion) {
-        this.anioPublicacion = anioPublicacion;
-    }
+	public void setAnioPublicacion(int anioPublicacion) {
+		this.anioPublicacion = anioPublicacion;
+	}
 
-    public Categoria getCategoria() {
-        return categoria;
-    }
+	public Categoria getCategoria() {
+		return categoria;
+	}
 
-    public void setCategoria(Categoria categoria) {
-        this.categoria = categoria;
-    }
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
 
-    public boolean isDisponible() {
-        return disponible;
-    }
+	public boolean isDisponible() {
+		return disponible;
+	}
 
-    public void setDisponible(boolean disponible) {
-        this.disponible = disponible;
-    }
-    
-    /******************************************************/
+	public void setDisponible(boolean disponible) {
+		this.disponible = disponible;
+	}
+
+	/******************************************************/
 
 	// parecido a usar toString
 	public void mostrarDetalles() {
@@ -117,4 +117,3 @@ public class Libro {
 	}
 
 }
-
