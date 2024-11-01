c4ae28b5a01cbaa3e2085aaa39c84c74c5339c78
Gerardo <gerarleonce-0703@outlook.es>
2024-11-01 16:57:47 -0500

    Se ajustó algunas entradas de salidas ya no necesarios

--- src/main/java/base_de_datos/Coneccion.java
+++ src/main/java/base_de_datos/Coneccion.java
@@ -9,7 +9,7 @@ public class Coneccion {
 		
 		private static String driver = "com.mysql.cj.jdbc.Driver";
 		private static String usuario = "root";
-		private static String password = "75212347";
+		private static String password = "75613247";
 		
 		// URL de la base de datos con ajuste para evitar advertencias de zona horaria y SSL
 		private static String url = "jdbc:mysql://localhost:3306/bookmaster?useSSL=false&serverTimezone=UTC";
@@ -22,7 +22,7 @@ public class Coneccion {
 				Class.forName(driver);
 				// Intentamos conectar a la base de datos usando DriverManager
 				con = DriverManager.getConnection(url, usuario, password);
-				System.out.println("Conectado a MySQL");
+				// System.out.println("Conectado a MySQL");
 			} catch (ClassNotFoundException e) {
 				// Capturamos e imprimimos cualquier error que ocurra al intentar cargar el driver
 				e.printStackTrace();
