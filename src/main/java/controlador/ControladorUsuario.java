081fdbcbd01735f15e262cb48dea7007ca6322b3
Gerardo <gerarleonce-0703@outlook.es>
2024-10-31 21:23:17 -0500

    Se agregó métodos como autenticar y registrar

--- src/main/java/controlador/ControladorUsuario.java
+++ src/main/java/controlador/ControladorUsuario.java
@@ -1,5 +1,27 @@
 package controlador;
 
-public class ControladorUsuario {
+import modelo.Usuario;
+import java.util.ArrayList;
+import java.util.List;
 
+public class ControladorUsuario {
+    private List<Usuario> usuarios = new ArrayList<>();
+
+    public Usuario autenticarUsuario(String correo, String contrasena) {
+        for (Usuario usuario : usuarios) {
+            if (usuario.getEmail().equals(correo) && usuario.getPassw().equals(contrasena)) {
+                return usuario;
+            }
+        }
+        return null;
+    }
+
+    public void registrarUsuario(String nombre, String apellido, String correo, int dni, String contrasena, String rol) {
+        Usuario nuevoUsuario = new Usuario(nombre, apellido, correo, dni, rol);
+        nuevoUsuario.setPassw(contrasena);
+        usuarios.add(nuevoUsuario);
+    }
+    
 }
+
+
