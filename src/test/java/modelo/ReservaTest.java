eeec766c68ae6bccb42b1b139686823b143cedb6
Gerardo <gerarleonce-0703@outlook.es>
2024-11-09 17:49:20 -0500

    Se dio la funcionalidad de cliquear lleve a otra ventana

--- src/test/java/modelo/ReservaTest.java
+++ src/test/java/modelo/ReservaTest.java
@@ -1,4 +1,4 @@
-package modelo;
+/*package modelo;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDateTime;
 
 public class ReservaTest {
-   /* private Reserva reserva;
+    private Reserva reserva;
     private Usuario usuario;
     private Libro libro;
 
@@ -58,6 +58,6 @@ public class ReservaTest {
         assertTrue(reservaString.contains("Reserva:"));
         assertTrue(reservaString.contains("El Quijote"));
         assertTrue(reservaString.contains("Juan"));
-    }*/
-}
+    }
+}*/
 
