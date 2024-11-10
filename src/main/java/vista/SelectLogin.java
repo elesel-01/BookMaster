14333fa8573247deca2bcc146a4b13e120f76e84
Gerardo <gerarleonce-0703@outlook.es>
2024-11-10 13:44:22 -0500

    Adaptandolo a gridbaglayout

--- src/main/java/vista/SelectLogin.java
+++ src/main/java/vista/SelectLogin.java
@@ -1,9 +1,11 @@
-
 package vista;
 
 import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.ImageIcon;
@@ -31,17 +33,30 @@ public class SelectLogin extends JFrame {
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
         setContentPane(contentPane);
 
-        JPanel selectLoginPanel = new JPanel();
+        JPanel selectLoginPanel = new JPanel(new GridBagLayout());
         selectLoginPanel.setBackground(new Color(39, 91, 131));
-        selectLoginPanel.setLayout(null);
         contentPane.add(selectLoginPanel, "SelectLogin");
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(10, 10, 10, 10);
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.CENTER;
+
+        JLabel lblNewLabel_4 = new JLabel("BOOKMASTER");
+        lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 32));
+        selectLoginPanel.add(lblNewLabel_4, gbc);
+
+        gbc.gridy++;
+        gbc.gridwidth = 1;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
 
         JButton btnIniciarSecin = new JButton("Iniciar sesión como Administrador");
         btnIniciarSecin.setForeground(Color.BLACK);
         btnIniciarSecin.setFont(new Font("Tahoma", Font.PLAIN, 15));
         btnIniciarSecin.setBackground(new Color(192, 192, 192));
-        btnIniciarSecin.setBounds(144, 213, 287, 39);
-        selectLoginPanel.add(btnIniciarSecin);
+        selectLoginPanel.add(btnIniciarSecin, gbc);
 
         btnIniciarSecin.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -49,28 +64,30 @@ public class SelectLogin extends JFrame {
             }
         });
 
-        JLabel lblNewLabel_4 = new JLabel("BOOKMASTER");
-        lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 32));
-        lblNewLabel_4.setBounds(158, 93, 239, 29);
-        selectLoginPanel.add(lblNewLabel_4);
+        gbc.gridy++;
 
         JButton btnNewButton = new JButton("Iniciar sesión como Usuario");
         btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 15));
         btnNewButton.setForeground(new Color(0, 0, 0));
         btnNewButton.setBackground(new Color(192, 192, 192));
-        btnNewButton.setBounds(144, 280, 287, 39);
-        selectLoginPanel.add(btnNewButton);
+        selectLoginPanel.add(btnNewButton, gbc);
+
+        gbc.gridx++;
+        gbc.gridy = 1;
+        gbc.gridheight = 2;
+        gbc.fill = GridBagConstraints.BOTH;
 
         JLabel lblNewLabel = new JLabel("");
         lblNewLabel.setIcon(new ImageIcon("src\\main\\java\\Imagen\\Captura de pantalla 2024-11-01 211115 (1).png"));
-        lblNewLabel.setBounds(576, 78, 260, 352);
-        selectLoginPanel.add(lblNewLabel);
+        selectLoginPanel.add(lblNewLabel, gbc);
+
+        gbc.gridx++;
+        gbc.gridheight = 2;
 
         JLabel lblNewLabel_1 = new JLabel("");
         lblNewLabel_1.setForeground(new Color(104, 105, 106));
         lblNewLabel_1.setIcon(new ImageIcon("src\\main\\java\\Imagen\\Captura de pantalla 2024-11-01 211334 (1).png"));
-        lblNewLabel_1.setBounds(0, 0, 576, 512);
-        selectLoginPanel.add(lblNewLabel_1);
+        selectLoginPanel.add(lblNewLabel_1, gbc);
 
         Login loginPanel = new Login(cardLayout, contentPane);
         contentPane.add(loginPanel, "Login");
