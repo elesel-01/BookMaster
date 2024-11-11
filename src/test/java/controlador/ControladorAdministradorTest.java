5dbb1404d9d2851a91f479b3539ea05f32c6d2b9
jerson-null <jersonvalquivargas@gmail.com>
2024-11-10 22:26:30 -0500

    funciono un test alfin mrd

--- src/test/java/controlador/ControladorAdministradorTest.java
+++ src/test/java/controlador/ControladorAdministradorTest.java
@@ -1,7 +1,8 @@
+
 package controlador;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -15,55 +16,51 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import base_de_datos.Coneccion;
-import controlador.ControladorAdministrador;
-import modelo.Libro;
-import modelo.Usuario;
 
 class ControladorAdministradorTest {
-	
-	
+
 	@Mock
 	private Coneccion coneccion;
-	
+
+	@Mock
+	private Connection connection;
+
 	@Mock
 	private PreparedStatement ps;
-	
+
 	@InjectMocks
-	private	 ControladorAdministrador controlador;
-	
+	private ControladorAdministrador controlador;
+
 	@BeforeEach
 	void setUp() throws Exception {
 		MockitoAnnotations.openMocks(this);
-		
-		when(coneccion.getConnection()).thenReturn(coneccion.getConnection());
-		when(coneccion.getConnection().prepareStatement(any(String.class))).thenReturn(ps);
-		
+
+		when(coneccion.getConnection()).thenReturn(connection);
+		when(connection.prepareStatement(anyString())).thenReturn(ps);
 	}
 
 	@Test
 	void testRegistrarLibro() throws Exception {
-		int idEstanteria= 5;
+		int idEstanteria = 5;
 		String titulo = "Harry Potter";
 		String autor = "J.K Rowling";
 		String editorial = "Salamandra";
 		int anioPublicacion = 2021;
-		String Categoria = "Fantasia";
+		String categoria = "Fantasia";
 		Boolean disponible = true;
-		
-		controlador.registrarLibro(idEstanteria, titulo, autor, editorial, anioPublicacion, Categoria,
-				disponible);
-		
-		((Connection) verify(coneccion)).prepareStatement("INSERT INTO libro(idEstanteria, titulo, autor, editorial, anioPublicacion, categoria, disponible) VALUES(?,?,?,?,?,?,?)");
+
+		controlador.registrarLibro(idEstanteria, titulo, autor, editorial, anioPublicacion, categoria, disponible);
+
+		verify(connection).prepareStatement(
+				"INSERT INTO libro(idEstanteria, titulo, autor, editorial, anioPublicacion, categoria, disponible) VALUES(?,?,?,?,?,?,?)");
 		verify(ps).setInt(1, idEstanteria);
 		verify(ps).setString(2, titulo);
 		verify(ps).setString(3, autor);
 		verify(ps).setString(4, editorial);
 		verify(ps).setInt(5, anioPublicacion);
-		verify(ps).setString(6, Categoria);
+		verify(ps).setString(6, categoria);
 		verify(ps).setBoolean(7, disponible);
 		verify(ps).execute();
-		
-		
 	}
 
 	@Test
@@ -90,5 +87,4 @@ class ControladorAdministradorTest {
 	void testEliminarUsuario() {
 		fail("Not yet implemented");
 	}
-
 }
