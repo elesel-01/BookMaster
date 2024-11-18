e0f69d03c95fea007efbd6996053b8fa4ec4d579
jerson-null <jersonvalquivargas@gmail.com>
2024-11-18 14:31:49 -0500

    FINAL FINAL FINAL

--- src/main/java/modelo/EstadoPrestamo.java
+++
@@ -1,9 +0,0 @@
-package modelo;
-/*
- * Enumerado que representa los posibles estados de un prestamo.
- * Es después de que un usuario haya reservado un libro y se le haya entregado.
- */
-public enum EstadoPrestamo {
-	ACTIVO,
-    ATRASADO
-}
--- src/main/java/modelo/Prestamos.java
+++ src/main/java/modelo/Prestamos.java
@@ -11,7 +11,6 @@ public class Prestamos {
 	private Usuario usuario;
 	private LocalDateTime fechaPrestamo; // fecha y hora en la que comienza el préstamo
 	private LocalDateTime fechaDevolucion; // fecha límite para la devolución del libro
-	private EstadoPrestamo estado; // Estado del préstamo (ACTIVO, DEVUELTO, ATRASADO, FINALIZADO)
 
 	public Prestamos(int id, Libro libro, Usuario usuario, LocalDateTime fechaPrestamo, int duracionDias) {
 		this.id = id;
@@ -21,7 +20,6 @@ public class Prestamos {
 		// La fecha de devolución se calculará usado duracionDias, que deberá ser
 		// proporcionada cuando se cree el objeto
 		this.fechaDevolucion = fechaPrestamo.plusDays(duracionDias);
-		this.estado = EstadoPrestamo.ACTIVO; // Al crear un préstamo, su estado inicial es ACTIVO
 	}
 
 	public int getId() {
@@ -62,47 +60,6 @@ public class Prestamos {
 
 	public void setFechaDevolucion(LocalDateTime fechaDevolucion) {
 		this.fechaDevolucion = fechaDevolucion;
-	}
-
-	public EstadoPrestamo getEstado() {
-		return estado;
-	}
-
-	public void setEstado(EstadoPrestamo estado) {
-		this.estado = estado;
-	}
-
-
-	public double calcularMulta() {
-		if (estado == EstadoPrestamo.ACTIVO) {
-			// Calcular los días de retraso si la fecha límite ya pasó
-			long diasRetraso = ChronoUnit.DAYS.between(fechaDevolucion, LocalDateTime.now());
-			if (diasRetraso > 0) {
-				return diasRetraso * 2.0; // Multa por 2 solsitos por día de retraso
-			}
-		}
-		return 0.0; // Si no hay retraso, la multa es cero
-	}
-
-	// Método para verificar si el préstamo está vencido
-	public boolean estaVencido() {
-		// Un préstamo está vencido si la fecha límite ya pasó y el estado sigue siendo
-		// ACTIVO
-		return LocalDateTime.now().isAfter(fechaDevolucion) && estado == EstadoPrestamo.ACTIVO;
-	}
-
-	// Método para obtener las fechas formateadas en un string
-	public String getFechasFormateadas() {
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm", Locale.of("es", "ES"));
-
-		return String.format("\n\tFecha de préstamo: %s\n\tFecha de devolución: %s", fechaPrestamo.format(formatter),
-				fechaDevolucion.format(formatter));
-	}
-
-	@Override
-	public String toString() {
-		return "Prestamo:" + "\n\tLibro = " + libro.getTitulo() + "\n\tUsuario = " + usuario.getNombre()
-				+ getFechasFormateadas() + "\n\tEstado = " + estado + "\nMulta = " + calcularMulta();
 	}
 
 }
